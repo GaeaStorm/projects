a1a4c05ec63d865a6a1172245cc6658159cc7aeb
GaiSawant <grs7@rice.edu>
2020-08-29 07:04:47 +0530

    Updates

---
+++ precache-manifest.0dbc408b5e9f244f8a334585fe47c87d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa41bccef4d4961a47c891f9965353c",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "5c8b4bee006e5d022d4d",
+    "url": "/projects/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "eac3fc6aacdb80f67ba2",
+    "url": "/projects/static/css/main.fe080f2a.chunk.css"
+  },
+  {
+    "revision": "5c8b4bee006e5d022d4d",
+    "url": "/projects/static/js/2.39104ba1.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/projects/static/js/2.39104ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac3fc6aacdb80f67ba2",
+    "url": "/projects/static/js/main.e15df184.chunk.js"
+  },
+  {
+    "revision": "f639c188cb940516bcf1",
+    "url": "/projects/static/js/runtime-main.da0864a9.js"
+  }
+]);
